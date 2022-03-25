8b51116a810d7c7d894a9df4f868e05dc9ce111d
Andreas Jansson <Mr_Fobu@hotmail.com>
2022-03-25 14:27:15 +0100

    such progrezz

---
+++ render.js
@@ -0,0 +1,35 @@
+function getSearch() {
+  let searchParams = new URLSearchParams(window.location.search);
+  let searchTerm = searchParams.get("search");
+  console.log(searchTerm);
+  search(searchTerm);
+}
+
+async function search(term) {
+  let response = await fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?query=${term}&apiKey=d70af83541ae424ea49d502f873f9311`
+  );
+  let json = await response.json();
+  console.log(json);
+  console.log(json);
+  render(json.results);
+}
+
+let container = document.getElementById("searchResults");
+function render(objectList) {
+  console.log("render");
+  console.log(objectList);
+  for (let i = 0; i < objectList.length; i++) {
+    let item = document.createElement("div");
+    item.classList.add("result");
+    let img = document.createElement("img");
+    img.classList.add("result_image");
+    img.src = objectList[i].image;
+    let title = document.createElement("h2");
+    title.innerText = objectList[i].title;
+    item.append(img);
+    item.append(title);
+    container.append(item);
+  }
+}
+getSearch();
