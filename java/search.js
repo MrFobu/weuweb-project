c1314a9402ad93c210a374add535d85d9693b3ef
Andreas Jansson <Mr_Fobu@hotmail.com>
2022-05-13 13:51:44 +0200

    -

--- java/search.js
+++ java/search.js
@@ -1,5 +1,5 @@
 let searchText = document.getElementById("searchBox");
-
+let button = document.getElementById("search");
 searchText.onkeydown = function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
@@ -7,3 +7,8 @@ searchText.onkeydown = function (event) {
     window.location.href = `search.html?search=${searchTerm}`;
   }
 };
+
+button.addEventListener("click", function () {
+  let searchTerm = searchText.value;
+  window.location.href = `search.html?search=${searchTerm}`;
+});
